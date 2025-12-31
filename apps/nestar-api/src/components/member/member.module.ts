ff3b6728c28271de1e05760e491175d28df9075d
komil14 <abdulvokhidovkomiljon@gmail.com>
2025-12-31 15:54:20 +0900

    feat: create member schema model

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberResolver } from './member.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import MemberSchema from '../../schemas/Member.model';
 
 @Module({
-  providers: [MemberService, MemberResolver]
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }])],
+	providers: [MemberService, MemberResolver],
 })
 export class MemberModule {}
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class MemberService {
+	constructor(@InjectModel('Member') private memberModel: Model<null>) {}
 	public async signUp(): Promise<string> {
 		return 'User signed up';
 	}
